eea1c5b815963822f08688821ab3dc7da4655e42
ybmadhu <ybmadhu707@gmail.com>
2021-06-26 01:58:40 +0530

    initial commit

---
+++ vars/common-seed.groovy
@@ -0,0 +1,25 @@
+def baseUrl = "https://github.com/ybmadhu/"
+def repoName = "$reponame"
+def gitRepoUrl= baseUrl + repoName + '.git'
+def jobName = "$reponame"
+def call(jobName, gitRepoUrl) {
+  pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                            //credentials('credentailsID')
+                        }
+                        branches('master')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
+}
