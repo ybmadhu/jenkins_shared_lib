8ed29215b669ad6b207d847ed18c49f6ff2ed8e2
ybmadhu <ybmadhu707@gmail.com>
2021-06-26 05:14:16 +0530

    added shared lib

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -17,9 +17,6 @@ def jobName = "$reponame"
                         }
                     }
                 }
-                triggers {
-                  cron("H/1 * * * *")
-                 }
                 scriptPath("Jenkinsfile")
             }
         }
