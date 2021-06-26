4d3264976bd61cfb17edc51d533c2a59f0452158
ybmadhu <ybmadhu707@gmail.com>
2021-06-27 00:07:46 +0530

    added poll scm

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -3,6 +3,19 @@ def repoName = "$reponame"
 def gitRepoUrl= baseUrl + repoName + '.git'
 def jobName = "$reponame"
   pipelineJob(jobName) {
+        properties {
+         pipelineTriggers {
+          triggers {
+            pollSCM {
+              scmpoll_spec('H/1 * * * *')
+              ignorePostCommitHooks(true)
+            }
+          }
+         }
+        }
+        logRotator{
+         numToKeep(5)
+        }
         definition {
             cpsScm {
                 scm {
